2d831cf51b14803b46d94aeeda55ed6a5e96160a
thearyadev <personal@aryankothari.dev>
2023-08-09 01:43:19 -0400

    minor changes

--- frontend/src/components/balance.tsx
+++ frontend/src/components/balance.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import CrystalSvg from '../assets/crystal.png';
 import { Stack } from '@mui/material';
 import Tooltip from '@mui/material/Tooltip';
+import { Balance as CrystalBalance } from '../models';
 
-export const Balance = ({ value }: { value: number }) => {
+export const Balance = ({ value }: { value: CrystalBalance }) => {
+    const verb = value.today > 0 ? 'Earned' : 'Spent';
+    const message = `${verb} ${Math.abs(value.today)} Crystals Today!`;
     return (
-        <Tooltip title="369 Crystals Earned Today!" placement='top'>
+        <Tooltip title={message} placement='top'>
 
             <Stack direction="row" spacing={1} alignItems='center'>
                 <img className='crystal-icon' src={CrystalSvg} />
-                <h1 className='crystal-balance'>{value}</h1>
+                <h1 className='crystal-balance'>{value.crystals}</h1>
             </Stack>
         </Tooltip>
     )

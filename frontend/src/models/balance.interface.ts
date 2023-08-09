09da30f4482852bcc8325d26a503619376214899
thearyadev <personal@aryankothari.dev>
2023-08-09 01:44:01 -0400

    balance interface

---
+++ frontend/src/models/balance.interface.ts
@@ -0,0 +1,4 @@
+export interface Balance {
+    crystals: number;
+    today: number;
+}
--- frontend/src/models/index.ts
+++ frontend/src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './transaction_item.interface';
 export * from './transaction_item_tier.interface';
 export * from './transaction.interface';
+export * from './balance.interface';

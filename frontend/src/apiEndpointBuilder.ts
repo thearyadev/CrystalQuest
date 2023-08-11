0223c2d9838a6242176cad3ef726592ddf9f60cc
thearyadev <personal@aryankothari.dev>
2023-08-10 23:37:50 -0400

    use correct address

--- frontend/src/apiEndpointBuilder.ts
+++ frontend/src/apiEndpointBuilder.ts
@@ -1,5 +1,11 @@
 export const apiEndpoint: string = "http://localhost:8000"
 
 export default function buildUrl(route: string): string {
-    return apiEndpoint + route
+
+
+    if (process.env.NODE_ENV === "development") {
+        return apiEndpoint + route
+    } else {
+        return route
+    }
 }

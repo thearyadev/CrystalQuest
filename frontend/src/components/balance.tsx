81b421efa219037d35a9b714cfce084c604f4556
thearyadev <personal@aryankothari.dev>
2023-08-17 17:04:41 -0400

    change h1 to h2 typography component

--- frontend/src/components/balance.tsx
+++ frontend/src/components/balance.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CrystalSvg from '../assets/crystal.png';
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import Tooltip from '@mui/material/Tooltip';
 import { Balance as CrystalBalance } from '../models';
 import { useEffect, useState } from 'react';
@@ -9,11 +9,12 @@ export const Balance = ({ value }: { value: CrystalBalance }) => {
 
     return (
 
-        <Stack direction="row" spacing={1} alignItems='center'>
+        <Stack direction="row" spacing={1} alignItems='center' sx={{display: "flex", maxWidth: "100%", overflow: "hidden"}}>
             <img className='crystal-icon' src={CrystalSvg} />
-            <h1 className={`crystal-balance`}>
+            <Typography variant='h2' sx={{flexShrink: 1, whiteSpace: "nowrap"} }>
                 {value.crystals.toLocaleString()}
-            </h1>
+            </Typography>
         </Stack>
     )
 }
+
